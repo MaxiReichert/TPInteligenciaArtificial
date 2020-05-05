79d6279e93b84f2f43f02b08d56d245a18a90e8f
[MaxiReichert] <[maximilianoreichert@gmail.com]>
2020-05-05 19:01:17 -0300

    Ventana Principal.

--- TP Inteligencia Artificial/src/TPInteligenciaArtificial/Main.java
+++ TP Inteligencia Artificial/src/TPInteligenciaArtificial/Main.java
@@ -1,9 +1,71 @@
 package TPInteligenciaArtificial;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		VBox vb= new VBox(20);
+		vb.setAlignment(Pos.TOP_CENTER);
+		
+		Label lbTitulo= new Label("Agente Custodio");
+		lbTitulo.setFont(Font.font("Time New Romans", 21));
+		//lbTitulo.setAlignment(Pos.TOP_CENTER);
+		
+		HBox hb1= new HBox(10);
+		hb1.setAlignment(Pos.CENTER);
+		
+		Label lbMetodoBusqueda= new Label("Elija un metodo de busqueda");
+		lbMetodoBusqueda.setFont(Font.font("Arial", 12));
+		
+		ComboBox<String> metodosBusqueda= new ComboBox();
+		metodosBusqueda.getItems().add("Profundidad");
+		metodosBusqueda.getItems().add("Horizontal");
+		metodosBusqueda.getItems().add("Avara");
+		
+		hb1.getChildren().add(lbMetodoBusqueda);
+		hb1.getChildren().add(metodosBusqueda);
+		
+		HBox hb2= new HBox();
+		hb2.setAlignment(Pos.BOTTOM_CENTER);
+		
+		Button btn= new Button("Avanzar");
+		btn.setOnAction(new EventHandler<ActionEvent>(){
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				/*Aca se debe llamar al main de la IA
+				 * y crear la ventana del mapa
+				 */
+			}
+			 
+		});
+		
+		hb2.getChildren().add(btn);
+		
+		vb.getChildren().add(lbTitulo);
+		vb.getChildren().add(hb1);
+		vb.getChildren().add(hb2);
+		Scene scene= new Scene(vb, 350,200);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
 	}
 
